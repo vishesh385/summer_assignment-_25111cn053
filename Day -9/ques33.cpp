46b4e7b6da94250cc44d761a9a5bb442a754747a
vishesh385 <visheshagarwal338@gmail.com>
2026-06-10 00:51:47 +0530

    Added Day 9 solutions

---
+++ Day -9/ques33.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i,n,j;
+    cout<<"ENTER THE VALUE OF N"<<endl;
+    cin>>n;
+
+    for(i = n;i >=1 ; i--){
+        for(j =1;j<=i;j++ ){
+            cout<<"*";
+        }
+        cout<<" "<<endl;
+    }
+
+
+}
